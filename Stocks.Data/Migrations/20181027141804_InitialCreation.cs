c8d8b2511fc491cbccac2fb5858fa63c96e8de78
George Johnson <georgemjohnson11@gmail.com>
2018-10-27 08:19:07 -0600

    Migrations created  Prior to Database Migrations via 'dotnet ef migrations add InitialCreation --context StockDbContext'

---
+++ Stocks.Data/Migrations/20181027141804_InitialCreation.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Stocks.Data.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockHistories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    History = table.Column<DateTime>(nullable: false),
+                    Open = table.Column<short>(nullable: false),
+                    Close = table.Column<short>(nullable: false),
+                    Volume = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockHistories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    AccountId = table.Column<int>(nullable: false),
+                    Type = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Portfolios_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExchangeRates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CurrencyFromId = table.Column<string>(nullable: true),
+                    CurrencyToId = table.Column<string>(nullable: true),
+                    Ratio = table.Column<short>(nullable: false),
+                    RateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExchangeRates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_StockHistories_CurrencyFromId",
+                        column: x => x.CurrencyFromId,
+                        principalTable: "StockHistories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ExchangeRates_StockHistories_CurrencyToId",
+                        column: x => x.CurrencyToId,
+                        principalTable: "StockHistories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockSplits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    StockId = table.Column<string>(nullable: true),
+                    Factor = table.Column<short>(nullable: false),
+                    SplitTimestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockSplits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StockSplits_StockHistories_StockId",
+                        column: x => x.StockId,
+                        principalTable: "StockHistories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Earnings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Amount = table.Column<decimal>(nullable: false),
+                    EarningsDate = table.Column<DateTime>(nullable: false),
+                    FromStockHistoryId = table.Column<string>(nullable: true),
+                    FromPortfolioId = table.Column<int>(nullable: false),
+                    FromAccountId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Earnings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Earnings_Accounts_FromAccountId",
+                        column: x => x.FromAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Earnings_Portfolios_FromPortfolioId",
+                        column: x => x.FromPortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Earnings_StockHistories_FromStockHistoryId",
+                        column: x => x.FromStockHistoryId,
+                        principalTable: "StockHistories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PortfolioStocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    PortfolioId = table.Column<int>(nullable: false),
+                    StockId = table.Column<string>(nullable: true),
+                    PurchaseTime = table.Column<DateTime>(nullable: false),
+                    PurchaseQuantity = table.Column<short>(nullable: false),
+                    PurchasePrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PortfolioStocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PortfolioStocks_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PortfolioStocks_StockHistories_StockId",
+                        column: x => x.StockId,
+                        principalTable: "StockHistories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Earnings_FromAccountId",
+                table: "Earnings",
+                column: "FromAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Earnings_FromPortfolioId",
+                table: "Earnings",
+                column: "FromPortfolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Earnings_FromStockHistoryId",
+                table: "Earnings",
+                column: "FromStockHistoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_CurrencyFromId",
+                table: "ExchangeRates",
+                column: "CurrencyFromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExchangeRates_CurrencyToId",
+                table: "ExchangeRates",
+                column: "CurrencyToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolios_AccountId",
+                table: "Portfolios",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioStocks_PortfolioId",
+                table: "PortfolioStocks",
+                column: "PortfolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PortfolioStocks_StockId",
+                table: "PortfolioStocks",
+                column: "StockId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockSplits_StockId",
+                table: "StockSplits",
+                column: "StockId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Earnings");
+
+            migrationBuilder.DropTable(
+                name: "ExchangeRates");
+
+            migrationBuilder.DropTable(
+                name: "PortfolioStocks");
+
+            migrationBuilder.DropTable(
+                name: "StockSplits");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+
+            migrationBuilder.DropTable(
+                name: "StockHistories");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
